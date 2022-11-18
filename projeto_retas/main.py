30c95117aaa6c510448517e77c200b41f63efefb
Talisson Avila <talisson_avila@hotmail.com>
2022-11-18 13:22:32 -0300

    criando imagens

---
+++ projeto_retas/main.py
@@ -0,0 +1,9 @@
+# a distancia entre dois pontos é : d² = (x_B - x_A)² + (y_B - y_A)²
+
+from projeto_retas.graphic.graphic import shows_graphic
+from projeto_retas.pictures.picture import picture_generation
+
+ponto_a = (-1, 2)
+ponto_b = (3, -1)
+shows_graphic(ponto_a, ponto_b)
+picture_generation(ponto_a, ponto_b)
